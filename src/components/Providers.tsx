426aa6a529b737637d09ae3d1d4f15033d9361a2
Rahul Kumar <rkumar1@asiapac.altair.com>
2026-05-28 19:04:37 +0530

    removed unused code

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import { AuthProvider } from "@/context/AuthContext";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="data-theme" defaultTheme="dark" enableSystem={false}>
-      <AuthProvider>
-        {children}
-      </AuthProvider>
+      {children}
     </ThemeProvider>
   );
 }
